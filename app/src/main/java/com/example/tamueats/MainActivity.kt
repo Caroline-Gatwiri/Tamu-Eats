ff3c06453ac8dd55cd1520097e8f85c049b91818
Caroline-Gatwiri <caroline1gatwiri@gmail.com>
2025-02-10 16:57:33 +0300

    Setting up the bottom Navigation

--- app/src/main/java/com/example/tamueats/MainActivity.kt
+++ app/src/main/java/com/example/tamueats/MainActivity.kt
@@ -5,16 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.tamueats.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val bottomNavigation = binding.bnv
+        val navController = Navigation.findNavController(this, R.id.navigationFragment)
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
     }
 }
